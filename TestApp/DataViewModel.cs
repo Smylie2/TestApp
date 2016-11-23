14d4dac705235d66b5bc3ad113019c2b88219208
c4udex <roberto-tomicic@gmx.de>
2016-11-23 19:24:24 +0100

    Projektdateien hinzufügen.

---
+++ TestApp/DataViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    [ImplementPropertyChanged]
+    public class DataViewModel
+    {
+        public ObservableCollection<DataLine> Lines { get; set; } = new ObservableCollection<DataLine>()
+        {
+            new DataLine() { Variable1 = 1, Variable2 = 2 },
+            new DataLine() { Variable1 = 2, Variable2 = 2 },
+            new DataLine() { Variable1 = 3, Variable2 = 2 },
+            new DataLine() { Variable1 = 4, Variable2 = 2 },
+            new DataLine() { Variable1 = 3, Variable2 = 2 },
+            new DataLine() { Variable1 = 6, Variable2 = 2 }
+        };
+    }
+
+    [ImplementPropertyChanged]
+    public class DataLine
+    {
+        public int Variable1 { get; set; }
+        public int Variable2 { get; set; }
+    }
+}
---
+++ TestApp/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    [ImplementPropertyChanged]
+    public class MainViewModel
+    {
+        public DataViewModel DataViewModel { get; set; } = new DataViewModel();
+    }
+}
